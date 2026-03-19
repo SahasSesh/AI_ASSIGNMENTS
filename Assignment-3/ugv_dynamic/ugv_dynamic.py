41403f36e3b1b62c30e2c15c4b3f684f3a14ecc0
Sahas Sesh <sahaslearns@gmail.com>
2026-03-19 22:40:09 +0530

    Update ugv_dynamic.py

--- Assignment-3/ugv_dynamic/ugv_dynamic.py
+++ Assignment-3/ugv_dynamic/ugv_dynamic.py
@@ -1,1 +1,29 @@
+import random
 
+def update_obstacles(grid, prob=0.05):
+    size = len(grid)
+    for i in range(size):
+        for j in range(size):
+            if random.random() < prob:
+                grid[i][j] = 1
+    return grid
+
+def simulate(start, goal):
+    pos = start
+    grid = [[0]*20 for _ in range(20)]
+
+    while pos != goal:
+        print("Position:", pos)
+        grid = update_obstacles(grid)
+
+        x, y = pos
+        if x < goal[0]:
+            x += 1
+        elif y < goal[1]:
+            y += 1
+
+        pos = (x, y)
+
+    print("Reached Goal")
+
+simulate((0,0), (19,19))
